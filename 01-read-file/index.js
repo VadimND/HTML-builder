65420aabeaa1906844bb35d1ed1242236d77e955
VadimND <webzorka24@gmail.com>
2023-05-04 10:21:58 +0300

    feat: solve 1st task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,4 +1,4 @@
-var fs = require('fs'),
+/*var fs = require('fs'),
     path = require('path'),
     filePath = path.join(__dirname, './text.txt');
 fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
@@ -7,4 +7,13 @@ fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
     } else {
         console.log(err);
     }
+});*/
+
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, './text.txt');
+const rr = fs.createReadStream(filePath);
+
+rr.on('readable', () => {
+    console.log(`readable: ${rr.read()}`);
 });
