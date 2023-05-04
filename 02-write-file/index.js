58ef74fc78b6f7f49269e66e1e3e294560fd5251
VadimND <webzorka24@gmail.com>
2023-05-04 15:15:53 +0300

    feat: solve 2st task

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, './text123.txt');
+
+const readline = require('readline');
+const {
+    stdin: input,
+    stdout: output,
+} = require('process');
+
+let writeableStream = fs.createWriteStream(filePath);
+
+const rl = readline.createInterface({ input, output });
+
+rl.question('What do you think of Node.js? ', (answer) => {
+    if (answer == "exit" || answer == '') {
+        console.log(
+            `Bye?`
+        );
+        rl.close();
+    }
+    else {
+        writeableStream.write(answer);
+        console.log(
+            `Thank you for your valuable feedback: ${answer}`
+        );    
+        rl.close();   
+    }
+});
